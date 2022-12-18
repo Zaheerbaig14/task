7ce05451019bd306136a73855bdb5fb44618e380
Zaheer Baig <120157186+Zaheerbaig14@users.noreply.github.com>
2022-12-18 16:00:21 +0500

    Add files via upload

---
+++ task9.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main(){
+system("color 87");
+cout<<"  ------------------------------------- "<<endl;
+cout<<"      o      ^___^                      "<<endl;
+cout<<"        o   ( oo )'\'___________         "<<endl;
+cout<<"            (____)'\'           )'\'      "<<endl;
+cout<<"                  ||-------W   |        "<<endl;
+cout<<"                  ||          ||        "<<endl;
+}    
